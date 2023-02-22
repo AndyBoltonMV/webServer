2fbbe82f3bc1bf5c1f3acda6d0379b997c2cda8f
AndyBoltonMV <andrew.bolton@multiverse.io>
2023-02-22 12:17:41 +0000

    deleted lines

--- src/server.js
+++ src/server.js
@@ -1,49 +1,13 @@
 const express = require("express"); // Imported the express function
-const app = express(); // Captures the return value of express() in app
-const db = require("./db/connection");
-const { User } = require("./models");
+const app = express(); // Captures the return value of express() in app, this is the web server
+const db = require("./db/connection"); // Grabs the db constant from my connection file
+const { User } = require("./models"); // Grabs the User model from my models folder
 
-app.use("/andyWeb", express.static("public"));
-// .use is adding functionality to the app web server
-// "/andyWeb" is a router/path that I am creating, it doesn't point to a file/folder
-// express.static is an express method for serving static files
-// it takes a folder location from the parent directory
-
-app.post("/createUser", async (request, response) => {
-  console.log(request);
-  console.log(response);
-  const user = await User.create({
-    username: "AndyB",
-    email: "andyb@email.com",
-    password: "test123",
-  });
-  response.send(user);
-});
-
-// Day 2 request params
-
-app.get("/user/:id", async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-    if (!user) {
-      throw new Error("No user found");
-    }
-    res.status(200).send({ user });
-  } catch (error) {
-    res.status(404).send({ err: error.message });
-  }
-});
-
-/*
-response = await fetch...
-data = await response.json()...
-if (data.status === 404) {
-  do some error handling
-  alert(incorrect credentials)
-}
-*/
-
+// Tells my app to start listening on port 3000, any code under this will not run.
 app.listen(3000, async () => {
+  // This controller/handler will run as soon as app.listen starts listening on port 3000
   await db.sync();
   console.log("Server is running");
 });
+
+// Code here will not run unless app.listen fails
